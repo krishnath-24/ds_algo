7f53f95c89bc7209ef042fe96ca56d31dae8ec05
Krishnath <shadyanuj24@gmail.com>
2020-05-04 16:11:00 +0530

    count steps to 1

---
+++ DynamicProgramming/MinStepsTo1.java
@@ -0,0 +1,29 @@
+
+public class Solution {
+	
+	public static int countStepsTo1(int n){
+		
+	/* Your class should be named Solution.
+	 * Don't write main() function.
+	 * Don't read input, it is passed as function argument.
+	 * Return output and don't print it.
+	 * Taking input and printing output is handled automatically.
+	 */
+        
+        if(n <= 1) return 0;
+        
+        int ans = 1 + countStepsTo1(n - 1);
+        
+        if(n % 2 == 0){
+            ans = Math.min(ans, 1 + countStepsTo1(n / 2));
+        }
+        
+        if(n % 3 == 0){
+            ans = Math.min(ans, 1 + countStepsTo1(n / 3));
+        }
+        	
+        return ans;
+	
+	}
+	
+}
