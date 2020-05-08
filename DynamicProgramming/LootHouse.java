2bdf9615a6d15bd11d3dca76020d35a52e22cc7f
Krishnath <shadyanuj24@gmail.com>
2020-05-08 12:32:07 +0530

    Loot houses

---
+++ DynamicProgramming/LootHouse.java
@@ -0,0 +1,29 @@
+
+public class Solution {
+	public static int getMaxMoney(int arr[], int n){
+
+		/*Your class should be named Solution.
+		*Don't write main().
+	 	*Don't take input, it is passed as function argument.
+	 	*Don't print output.
+	 	*Taking input and printing output is handled automatically.
+		*/
+        
+        if(n == 0) return 0;
+        
+        if(n == 1) return arr[0];
+        
+        int[] dp = new int[n];
+        
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0],arr[1]);
+        
+        for(int i = 2; i < n; i++){
+            
+            dp[i] = Math.max(dp[i-2] + arr[i], dp[i-1]);
+        }
+        
+        return dp[n-1];
+
+	}
+}
